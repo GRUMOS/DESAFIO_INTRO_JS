ea22b052d759699861efa58ab74fe15d2d9726ec
MAO <mjose.araya@outlook.com>
2023-08-31 22:00:27 -0400

    DESAFIO_INTRO_JS

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+let initialPriceElement = document.querySelector('.initialPrice'); // variables con clases en el DOM
+let quantityElement = document.querySelector('.quantity');
+let masButton = document.querySelector('.mas');
+let menosButton = document.querySelector('.menos');
+let totalSection = document.querySelector('.total-section');
+let valorTotalElement = document.querySelector('.valor-total');
+  
+masButton.addEventListener('click', () => {  //al hacer click aumenta el precio 
+    quantityElement.innerHTML++;
+    initialPriceElement.innerHTML = quantityElement.innerHTML * initialPriceElement.innerHTML;
+});
+
+menosButton.addEventListener('click', () => { //al hacer click disminuye el precio 
+    quantityElement.innerHTML--;
+    initialPriceElement.innerHTML = quantityElement.innerHTML * initialPriceElement.innerHTML;
+});
+
